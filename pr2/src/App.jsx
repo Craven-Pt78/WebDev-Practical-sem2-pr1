65942926170074ef1174fdca53f6a8dc54c48bfd
Nihal Singh <craven.pt78@gmail.com>
2026-05-05 10:24:36 +0530

    First Commit

---
+++ pr2/src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Posts from JSONPlaceholder</h1>
+
+      {data.map((item) => (
+        <div key={item.id} style={{ border: "1px solid black", margin: "10px", padding: "10px" }}>
+          <h3>{item.title}</h3>
+          <p>{item.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
